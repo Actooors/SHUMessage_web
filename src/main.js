988bb3b195e9055345a748b5b4bb82993720ab2c
mzz2017 <m@mzz.pub>
2018-07-15 17:52:42 +0800

    chore: 引入scss支持

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import FastClick from 'fastclick'
 import router from './router'
 import App from './App'
+import { TransferDom } from 'vux'
+Vue.directive('transfer-dom', TransferDom)
 
-
+const FastClick = require('fastclick')
 FastClick.attach(document.body)
 
 Vue.config.productionTip = false
