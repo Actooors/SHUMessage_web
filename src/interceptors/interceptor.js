46c637946f628617ab9d8e504889a768bd2bf2bf
mzz2017 <m@mzz.pub>
2018-08-31 23:23:45 +0800

    fix(interceptor): 去掉Authorization中的bearer字段，与后端对接

--- src/interceptors/interceptor.js
+++ src/interceptors/interceptor.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 axios.interceptors.request.use((config) => {
   // let token = window.localStorage.getItem("token")
-  let token = `bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMDAwNzc1NCIsImV4cCI6MTUzNTg3MjQ1NCwiaWF0IjoxNTM1NDQwNDU0fQ.qNm_gP6ttuPTonXKBhilb_7OBYaOtgwaQy8fbhfURJ0UaGoX4u0SkxlBAitO_fDZAyATq8l1Pfp49eYNEzjl9g`
+  let token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMDAwNzc1NCIsImV4cCI6MTUzNTg3MjQ1NCwiaWF0IjoxNTM1NDQwNDU0fQ.qNm_gP6ttuPTonXKBhilb_7OBYaOtgwaQy8fbhfURJ0UaGoX4u0SkxlBAitO_fDZAyATq8l1Pfp49eYNEzjl9g`
   if (token) {
     config.headers.Authorization = token
   }
