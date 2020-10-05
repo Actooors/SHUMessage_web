90c2a7efc722d4e4274466b2bdd6bdf6aea79a4b
spero002 <72220614+spero002@users.noreply.github.com>
2020-10-05 16:17:38 +0530

    Update build.js

--- build/build.js
+++ build/build.js
@@ -1,5 +1,5 @@
 'use strict'
-require('./check-versions')()
+require('./check-versions');
 
 process.env.NODE_ENV = 'production'
 
@@ -9,9 +9,9 @@ const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
-const webpackConfig = require('./webpack.prod.conf')
+const webpackConfig = require('./webpack.prod.conf');
 
-const spinner = ora('building for production...')
+const spinner = ora('building for production...');
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
